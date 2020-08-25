ef31e870774f4df56f6b8ea3845c93c021297367
ISHAN MAKADIA <ishan.makadia@gmail.com>
2020-08-25 14:07:39 +0530

    Add Extra

---
+++ Extra/grader.js
@@ -0,0 +1,15 @@
+function average(arr){
+    let total = 0;
+    arr.forEach(element => {
+        total = total + element; 
+    });
+    let length = arr.length;
+    console.log(Math.round(total/length));
+}
+
+var scores = [90,98,89,100,100,86,94];
+average(scores);
+
+
+var scores2 = [40,65,77,82,80,54,73,63,95,49];
+average(scores2);
